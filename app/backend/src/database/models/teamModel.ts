33c1b73cf11d56f86d0816047034e96e3357d4ea
Bruno <bruno_bury@hotmail.com>
2023-11-01 16:47:28 -0300

    lint

--- app/backend/src/database/models/teamModel.ts
+++ app/backend/src/database/models/teamModel.ts
@@ -1,34 +1,34 @@
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
-  } from 'sequelize';
-  import db from '.';
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
   
-  class Team extends Model<InferAttributes<Team>, InferCreationAttributes<Team>> {
-    declare id: CreationOptional<number>;
-    declare teamName: string;
-  }
-  Team.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    teamName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      field: 'team_name',
-    },
-  }, {
-    sequelize: db,
-    modelName: 'Team',
-    timestamps: false,
-    underscored: true,
-  });
+class Team extends Model<InferAttributes<Team>, InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'Team',
+  timestamps: false,
+  underscored: true,
+});
   
-  export default Team;
+export default Team;
   
